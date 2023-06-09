a2d77895803d7f52b950832d72ca8faf7cf59e42
ngogatishvili <ngogatishvili@websmart.gmail.com>
2023-06-09 15:54:10 +0400

    fx

--- server/src/app.ts
+++ server/src/app.ts
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname,'..','public')));
 app.use('/api/v1',apiRouter);
 
 app.use('/*',(req:Request,res:Response)=>{
-    res.sendFile(path.join(__dirname,'..','public','index,html'))
+    res.sendFile(path.join(__dirname,'..','public','index.html'))
 })
 
 
