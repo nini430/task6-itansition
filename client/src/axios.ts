efb2f58ca73360fc99463ebb7b1c17b8b120bd7d
ngogatishvili <ngogatishvili@websmart.gmail.com>
2023-06-09 15:36:08 +0400

    fx

--- client/src/axios.ts
+++ client/src/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 const axiosApiInstance=axios.create({
-    baseURL:'https://email-system-w3fe.onrender.com/api/v1'
+    baseURL:"https://email-system-w3fe.onrender.com/api/v1"
 })
 
 export default axiosApiInstance;
