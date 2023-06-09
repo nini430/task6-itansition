8d1c5b4ba6d7c87c4ab5f87b5e209ca07bc97781
ngogatishvili <ngogatishvili@websmart.gmail.com>
2023-06-09 15:53:27 +0400

    fx

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,7 @@
 import express from 'express';
+import {Request,Response} from 'express';
 import cors from 'cors'
+import path from 'path';
 
 import apiRouter from './routes/api';
 
@@ -8,7 +10,12 @@ const app=express();
 app.use(express.json());
 app.use(cors())
 
+app.use(express.static(path.join(__dirname,'..','public')));
 app.use('/api/v1',apiRouter);
+
+app.use('/*',(req:Request,res:Response)=>{
+    res.sendFile(path.join(__dirname,'..','public','index,html'))
+})
 
 
 export default app;
